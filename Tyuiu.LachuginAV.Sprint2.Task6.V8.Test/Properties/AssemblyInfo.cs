24ac171bbb3457ec7d3965ff9abb351fff5d5faa
Tyuiu.LachuginAV@outlook.com <lavtemik@mail.ru>
2023-10-14 15:14:04 +0300

    Task6

---
+++ Tyuiu.LachuginAV.Sprint2.Task6.V8.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LachuginAV.Sprint2.Task6.V8.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LachuginAV.Sprint2.Task6.V8.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("65fd5f4f-b24e-48c3-abea-7c4866ebca06")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
