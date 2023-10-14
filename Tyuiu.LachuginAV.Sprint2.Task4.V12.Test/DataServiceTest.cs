b7abe4f182dfe22760e6a82bc9f9001c70a5b459
Tyuiu.LachuginAV@outlook.com <lavtemik@mail.ru>
2023-10-14 14:38:22 +0300

    Task4

--- Tyuiu.LachuginAV.Sprint2.Task4.V12.Test/DataServiceTest.cs
+++ Tyuiu.LachuginAV.Sprint2.Task4.V12.Test/DataServiceTest.cs
@@ -10,6 +10,7 @@ namespace Tyuiu.LachuginAV.Sprint2.Task4.V12.Test
         [TestMethod]
         public void TestMethod1()
         {
+           
             DataService ds = new DataService();
             double y = 1, x = 1;
             Assert.AreEqual(23.618, ds.Calculate(x, y));
